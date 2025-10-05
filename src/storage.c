9f58373fdc1fbaf60c94b23dc8507afad6a18590
Miguel Ángel Gómez Olarte <124186064+masalgaro@users.noreply.github.com>
2025-10-04 22:35:06 -0500

    Revirtiendo a version consistente de persistencia

--- src/storage.c
+++ src/storage.c
@@ -120,7 +120,7 @@ int storage_add(const char *value) {
 
     if (data_len <= 2) {
         // archivo vacío: []
-        snprintf(new_data, new_len, "[\n\t%s\n]", entry);
+        snprintf(new_data, new_len, "[%s]", entry);
     } else {
         // Crear una copia temporal para no modificar el buffer original
         char *temp_data = malloc(data_len + 1);
@@ -132,7 +132,7 @@ int storage_add(const char *value) {
         }
         strcpy(temp_data, data);
         temp_data[data_len-2] = '\0'; // quitar ']' de la copia
-        snprintf(new_data, new_len, "%s,\n\t%s\n]", temp_data, entry);
+        snprintf(new_data, new_len, "%s,%s]", temp_data, entry);
         free(temp_data);
     }
 
